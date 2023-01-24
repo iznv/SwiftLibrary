a40ab0acce4a8afb0da8b6404b92cc6a98dd7b78
Ivan Zinovev <zinovyev-ivan@yandex.ru>
2023-01-24 18:47:52 +0300

    Set iOS 14 as min version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftLibrary",
+    platforms: [.iOS(.v14)],
     products: [
         .library(name: "SwiftLibrary",
                  targets: ["SwiftLibrary"]),
