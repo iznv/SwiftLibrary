e78de8728abfe95f7ae35c56df8fa3c4f8afac64
Ivan Zinovev <zinovyev-ivan@yandex.ru>
2023-01-25 16:41:33 +0300

    Add new extensions

---
+++ Sources/SwiftLibraryCore/UIKit/Extensions/UIKit/UIImage/UIImage+SymbolWeight.swift
@@ -0,0 +1,16 @@
+//
+//  UIImage+SymbolWeight.swift
+//  
+//
+//  Created by Ivan Zinovyev on 25.01.2023.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    func with(weight: UIImage.SymbolWeight) -> UIImage {
+        return withConfiguration(UIImage.SymbolConfiguration(weight: weight))
+    }
+    
+}
---
+++ Sources/SwiftLibraryCore/UIKit/Extensions/UIKit/UIView/UIView+Preview.swift
@@ -0,0 +1,40 @@
+//
+//  UIView+Preview.swift
+//  
+//
+//  Created by Ivan Zinovyev on 25.01.2023.
+//
+
+import SwiftUI
+import UIKit
+
+extension UIView {
+
+    var preview: some View {
+        Preview(view: self)
+    }
+
+}
+
+// MARK: - Private
+
+private extension UIView {
+
+    struct Preview<V: UIView>: UIViewRepresentable {
+
+        // MARK: - Properties
+
+        let view: V
+        
+        // MARK: - UIViewRepresentable
+
+        func makeUIView(context: UIViewRepresentableContext<Preview<V>>) -> V {
+            return view
+        }
+
+        func updateUIView(_ uiView: V,
+                          context: UIViewRepresentableContext<Preview<V>>) { }
+
+    }
+
+}
