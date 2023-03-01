29c050edcc4e6f0784125869df3525c996fb4756
Ivan Zinovev <zinovyev-ivan@yandex.ru>
2023-03-01 14:46:51 +0300

    Remove UIKit specific things

--- Package.swift
+++ Package.swift
@@ -7,35 +7,10 @@ let package = Package(
     name: "SwiftLibrary",
     platforms: [.iOS(.v14)],
     products: [
-        .library(name: "SwiftLibraryCore",
-                 targets: ["SwiftLibraryCore"]),
-        .library(name: "SwiftLibraryUIKit",
-                 targets: ["SwiftLibraryUIKit"]),
-        .library(name: "SwiftLibraryRxCocoa",
-                 targets: ["SwiftLibraryRxCocoa"]),
-        .library(name: "SwiftLibraryTableKit",
-                 targets: ["SwiftLibraryTableKit"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/iznv/RxSwiftBinaries", from: "6.5.0"),
-        .package(url: "https://github.com/iznv/SnapKitBinaries", from: "5.6.0"),
-        .package(url: "https://github.com/iznv/TableKitBinaries", from: "2.11.0")
+        .library(name: "SwiftLibrary",
+                 targets: ["SwiftLibrary"])
     ],
     targets: [
-        .target(name: "SwiftLibraryCore"),
-        .target(name: "SwiftLibraryUIKit",
-                dependencies: [
-                    .product(name: "SnapKit", package: "SnapKitBinaries")
-                ]),
-        .target(name: "SwiftLibraryRxCocoa",
-                dependencies: [
-                    .product(name: "RxCocoa", package: "RxSwiftBinaries")
-                ]),
-        .target(name: "SwiftLibraryTableKit",
-                dependencies: [
-                    "SwiftLibraryCore",
-                    .product(name: "SnapKit", package: "SnapKitBinaries"),
-                    .product(name: "TableKit", package: "TableKitBinaries"),
-                ])
+        .target(name: "SwiftLibrary")
     ]
 )
--- Sources/SwiftLibraryUIKit/Helpers/Decorator/Decoration.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Decoration.swift
-//  
-//
-//  Created by Ivan Zinovyev on 24.01.2023.
-//
-
-public typealias Decoration<T> = (T) -> Void
