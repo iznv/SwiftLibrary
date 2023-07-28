f45e4ebad92d642b9fbcffb38105a13415b37f29
Ivan Zinovev <zinovyev-ivan@yandex.ru>
2023-07-28 19:51:35 +0300

    Replace binaries with original dependencies

--- Package.swift
+++ Package.swift
@@ -15,15 +15,15 @@ let package = Package(
                  targets: ["TableKitLibrary"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.5.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.6.0"),
         .package(url: "https://github.com/RxSwiftCommunity/RxGesture", from: "4.0.4"),
-        .package(url: "https://github.com/iznv/SnapKitBinaries", from: "5.6.0"),
-        .package(url: "https://github.com/iznv/TableKitBinaries", from: "2.11.0")
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.6.0"),
+        .package(url: "https://github.com/maxsokolov/TableKit", from: "2.11.0")
     ],
     targets: [
         .target(name: "UIKitLibrary",
                 dependencies: [
-                    .product(name: "SnapKit", package: "SnapKitBinaries")
+                    .product(name: "SnapKit", package: "SnapKit")
                 ]),
         .target(name: "RxCocoaLibrary",
                 dependencies: [
@@ -32,8 +32,8 @@ let package = Package(
                 ]),
         .target(name: "TableKitLibrary",
                 dependencies: [
-                    .product(name: "SnapKit", package: "SnapKitBinaries"),
-                    .product(name: "TableKit", package: "TableKitBinaries"),
+                    .product(name: "SnapKit", package: "SnapKit"),
+                    .product(name: "TableKit", package: "TableKit"),
                 ])
     ]
 )
