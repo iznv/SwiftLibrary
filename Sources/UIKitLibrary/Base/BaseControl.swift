be5a57878e73cd28f604c1415f66553162a7dbee
Ivan Zinovev <zinovyev-ivan@yandex.ru>
2023-08-01 21:58:15 +0300

    Add BaseControl

---
+++ Sources/UIKitLibrary/Base/BaseControl.swift
@@ -0,0 +1,56 @@
+//
+//  BaseControl.swift
+//  
+//
+//  Created by Ivan Zinovev on 01.08.2023.
+//
+
+import UIKit
+
+open class BaseControl: UIControl {
+    
+    // MARK: - Init
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    // MARK: - To Override
+    
+    open func commonInit() {
+        initialize()
+    }
+    
+    open func addViews() { }
+    
+    open func configureConstraints() { }
+    
+    open func configureAppearance() { }
+    
+    // MARK: - Overrides
+    
+    open override var isHighlighted: Bool {
+        didSet {
+            animate(isHighlighted: isHighlighted)
+        }
+    }
+    
+}
+
+// MARK: - Initialization
+
+extension BaseControl {
+    
+    func initialize() {
+        addViews()
+        configureConstraints()
+        configureAppearance()
+    }
+    
+}
