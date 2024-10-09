8fa0ee680dd71417492ffd619a4e72a797c94f0d
Ivan Zinovev <zinovyev-ivan@yandex.ru>
2024-10-09 22:31:39 +0300

    Add MainActor fro Configurator

--- Sources/SwiftLibrary/Helpers/Configurator/Configurator.swift
+++ Sources/SwiftLibrary/Helpers/Configurator/Configurator.swift
@@ -5,12 +5,14 @@
 //  Created by Ivan Zinovyev on 23.02.2023.
 //
 
+@MainActor
 public protocol Configurator {
     
     func configure()
     
 }
 
+@MainActor
 public extension Array where Element == Configurator {
     
     func configure() {
